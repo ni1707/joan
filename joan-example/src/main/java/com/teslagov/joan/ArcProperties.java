6821a60c796a813b601a9b90de990d702fa49e06
Kevin Chen <kevinmichaelchen@gmail.com>
2016-08-24 12:42:57 -0400

    Add docs for ArcProperties

--- joan-example/src/main/java/com/teslagov/joan/ArcProperties.java
+++ joan-example/src/main/java/com/teslagov/joan/ArcProperties.java
@@ -7,10 +7,29 @@ import com.teslagov.properties.PropertyEnum;
  */
 public enum ArcProperties implements PropertyEnum
 {
+	/**
+	 * The username of a Portal admin.
+	 */
 	USERNAME( "username" ),
+
+	/**
+	 * The password of a Portal admin.
+	 */
 	PASSWORD( "password" ),
+
+	/**
+	 * The Portal URL, e.g., https://my.web.adaptor.url/arcgis
+	 */
 	PORTAL_URL( "portalUrl" ),
+
+	/**
+	 * The port Portal is running on.
+	 */
 	PORT( "port" ),
+
+	/**
+	 * The referer to use when communicating with Portal's REST API.
+	 */
 	REFERER( "referer" );
 
 	private final String propertyName;
