02636ce12e86d64b0ae9d09994e1a14bd084b7ba
Kevin Chen <kevinmichaelchen@gmail.com>
2016-08-24 22:55:20 -0400

    create joan-arc module which ties together core, portal, and server

---
+++ joan-arc/src/main/java/com/teslagov/joan/ArcApi.java
@@ -0,0 +1,144 @@
+package com.teslagov.joan;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teslagov.joan.portal.group.Group;
+import com.teslagov.joan.portal.group.GroupCreator;
+import com.teslagov.joan.portal.group.GroupResponse;
+import com.teslagov.joan.portal.portal.PortalFetcher;
+import com.teslagov.joan.portal.portal.PortalResponse;
+import com.teslagov.joan.portal.token.PortalTokenFetcher;
+import com.teslagov.joan.portal.token.PortalTokenResponse;
+import com.teslagov.joan.portal.user.User;
+import com.teslagov.joan.portal.user.UserFetcher;
+import com.teslagov.joan.server.token.ServerTokenFetcher;
+import org.apache.http.client.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+/**
+ * @author Kevin Chen
+ */
+public class ArcApi
+{
+	private static final Logger logger = LoggerFactory.getLogger( ArcApi.class );
+
+	private static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
+
+	private final HttpClient httpClient;
+
+	private final ArcConfiguration arcConfiguration;
+
+	private final PortalTokenFetcher portalTokenFetcher = new PortalTokenFetcher();
+
+	private PortalTokenResponse portalTokenResponse;
+
+	private final PortalFetcher portalFetcher = new PortalFetcher();
+
+	private PortalResponse portalResponse;
+
+	private final UserFetcher userFetcher = new UserFetcher();
+
+	private final GroupCreator groupCreator = new GroupCreator();
+
+	private final ServerTokenFetcher serverTokenFetcher = new ServerTokenFetcher();
+
+	public ArcApi( HttpClient httpClient, ArcConfiguration arcConfiguration )
+	{
+		this.httpClient = httpClient;
+		this.arcConfiguration = arcConfiguration;
+	}
+
+	public void fetchToken()
+	{
+		portalTokenResponse = portalTokenFetcher.fetchToken( httpClient, arcConfiguration );
+		logger.debug( "PortalTokenResponse successful: {}", portalTokenResponse.isSuccess() );
+		logger.debug( "PortalTokenResponse toString: {}", portalTokenResponse );
+
+		LocalDateTime expirationDate = getTokenExpirationTime();
+		LocalDateTime now = LocalDateTime.now( ZONE_OFFSET );
+		logger.debug( "Current time is {}", now );
+		logger.debug( "Token will expire on {}", expirationDate );
+		logger.debug( "Token expires in {} seconds", now.until( expirationDate, ChronoUnit.SECONDS ) );
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion( JsonInclude.Include.NON_NULL );
+
+		try
+		{
+			logger.debug( "TOKEN SERIALIZED = {}", objectMapper.writeValueAsString( portalTokenResponse ) );
+		}
+		catch ( JsonProcessingException e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void getPortal()
+	{
+		portalResponse = portalFetcher.fetchPortal( httpClient, arcConfiguration, portalTokenResponse );
+		logger.debug( "Portal ID = {}", portalResponse.id );
+	}
+
+	public List<User> fetchUsers()
+	{
+		refreshPortalTokenIfNecessary();
+
+		if ( portalResponse == null )
+		{
+			getPortal();
+		}
+
+		return userFetcher.fetchUsers( httpClient, arcConfiguration, portalTokenResponse, portalResponse );
+	}
+
+	public GroupResponse createGroup( Group group )
+	{
+		refreshPortalTokenIfNecessary();
+		GroupResponse groupResponse = groupCreator.createGroup( httpClient, arcConfiguration, portalTokenResponse, group );
+		logger.debug( "GROUP ACCESS = {}", groupResponse.group.access );
+
+		return groupResponse;
+	}
+
+	// TODO maybe move this to a decorator class
+	private void refreshPortalTokenIfNecessary()
+	{
+		if ( isTokenExpired() )
+		{
+			fetchToken();
+		}
+	}
+
+	private boolean isTokenExpired()
+	{
+		LocalDateTime now = LocalDateTime.now( ZONE_OFFSET );
+		LocalDateTime expirationTime = getTokenExpirationTime();
+
+		if ( portalTokenResponse == null )
+		{
+			logger.debug( "Portal token is null... fetching one now..." );
+			return false;
+		}
+
+		if ( now.isAfter( expirationTime ) )
+		{
+			logger.debug( "Current time {} is after token expiration time {}", now, expirationTime );
+			return false;
+		}
+
+		return true;
+	}
+
+	private LocalDateTime getTokenExpirationTime()
+	{
+		long expiresEpochMs = portalTokenResponse.getExpires();
+		return LocalDateTime.ofEpochSecond( expiresEpochMs / 1000, 0, ZONE_OFFSET );
+	}
+}
--- joan-example/src/main/java/com/teslagov/joan/Main.java
+++ joan-example/src/main/java/com/teslagov/joan/Main.java
@@ -1,90 +1,40 @@
 package com.teslagov.joan;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teslagov.joan.portal.group.Group;
 import com.teslagov.joan.portal.group.GroupAccess;
-import com.teslagov.joan.portal.group.GroupCreator;
-import com.teslagov.joan.portal.group.GroupResponse;
 import com.teslagov.joan.portal.group.GroupSortField;
-import com.teslagov.joan.portal.portal.PortalFetcher;
-import com.teslagov.joan.portal.portal.PortalResponse;
-import com.teslagov.joan.portal.token.PortalTokenFetcher;
-import com.teslagov.joan.portal.token.PortalTokenResponse;
-import com.teslagov.joan.portal.user.UserFetcher;
-import com.teslagov.joan.server.token.ServerTokenFetcher;
-import com.teslagov.joan.server.token.ServerTokenResponse;
 import org.apache.http.client.HttpClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Arrays;
+import static com.teslagov.joan.portal.group.GroupBuilder.newGroup;
 
 /**
  * @author Kevin Chen
  */
 public class Main
 {
-	private static final Logger logger = LoggerFactory.getLogger( Main.class );
-
 	public static void main( String[] args )
 	{
 		ArcConfiguration arcConfiguration = ArcConfigurationFactory.createArcConfiguration();
 
 		HttpClient httpClient = TrustingHttpClientFactory.createVeryUnsafePortalHttpClient( arcConfiguration );
 
-		PortalTokenFetcher portalTokenFetcher = new PortalTokenFetcher();
+		ArcApi arcApi = new ArcApi( httpClient, arcConfiguration );
+		arcApi.fetchToken();
+		arcApi.fetchUsers();
 
-		PortalTokenResponse portalTokenResponse = portalTokenFetcher.fetchToken( httpClient, arcConfiguration );
-
-		logger.debug( "PortalTokenResponse successful: {}", portalTokenResponse.isSuccess() );
-		logger.debug( "PortalTokenResponse toString: {}", portalTokenResponse );
-
-		long expiresEpochMs = portalTokenResponse.getExpires();
-		LocalDateTime localDateTime = LocalDateTime.ofEpochSecond( expiresEpochMs / 1000, 0, ZoneOffset.UTC );
-		logger.debug( "Current time = {}", LocalDateTime.now( ZoneOffset.UTC ) );
-		logger.debug( "Token expires at {}", localDateTime );
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.setSerializationInclusion( JsonInclude.Include.NON_NULL );
-
-		try
-		{
-			logger.debug( "TOKEN SERIALIZED = {}", objectMapper.writeValueAsString( portalTokenResponse ) );
-		}
-		catch ( JsonProcessingException e )
-		{
-			e.printStackTrace();
-		}
-
-		PortalFetcher portalFetcher = new PortalFetcher();
-		PortalResponse portalResponse = portalFetcher.fetchPortal( httpClient, arcConfiguration, portalTokenResponse );
-		logger.debug( "Portal ID = {}", portalResponse.id );
-
-		UserFetcher userFetcher = new UserFetcher();
-		userFetcher.fetchUsers( httpClient, arcConfiguration, portalTokenResponse, portalResponse );
-
-		GroupCreator groupCreator = new GroupCreator();
-		Group group = new Group();
-		group.title = "Kevin's Test Group 4";
-		group.description = "A test group owned by Kevin";
-		group.snippet = "snippet...";
-		group.tags = Arrays.asList( "tag1", "tag2" );
-		group.phone = "Contact info";
-		group.access = GroupAccess.PUBLIC;
-		group.sortField = GroupSortField.TITLE;
-		group.sortOrder = SortOrder.ASCENDING;
-		group.isViewOnly = true;
-		group.isInvitationOnly = false;
-		group.thumbnail = "";
-		GroupResponse groupResponse = groupCreator.createGroup( httpClient, arcConfiguration, portalTokenResponse, group );
-		logger.info( "GROUP ACCESS = {}", groupResponse.group.access );
-
-		// FETCH TOKEN FOR SERVER ADMIN API
-//		ServerTokenFetcher serverTokenFetcher = new ServerTokenFetcher();
-//		ServerTokenResponse serverTokenResponse = serverTokenFetcher.fetchServer( httpClient, arcConfiguration, 120 );
+		Group group = newGroup()
+			.title( "Kevin's Test Group 5" )
+			.description( "A test group owned by Kevin" )
+			.snippet( "snippet..." )
+			.tag( "tag1" ).tag( "tag2" ).tag( "tag3" )
+			.phone( "1600 Pennsylvania Ave" )
+			.access( GroupAccess.PUBLIC )
+			.sortField( GroupSortField.TITLE )
+			.sortOrder( SortOrder.ASCENDING )
+			.isViewOnly( true )
+			.isInvitationOnly( false )
+			.thumbnail( "" )
+			.build();
+		arcApi.createGroup( group );
 	}
 }
