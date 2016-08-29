2d17c3140dcb803a8d151ea1636a97e0f2e5cc6d
Kevin Chen <kevinmichaelchen@gmail.com>
2016-08-29 16:36:23 -0400

    ArcPortalLogApi extends AbstractArcRestApi

--- joan-arc/src/main/java/com/teslagov/joan/api/portal/ArcPortalLogApi.java
+++ joan-arc/src/main/java/com/teslagov/joan/api/portal/ArcPortalLogApi.java
@@ -1,21 +1,26 @@
 package com.teslagov.joan.api.portal;
 
+import com.teslagov.joan.api.AbstractArcRestApi;
 import com.teslagov.joan.core.ArcConfiguration;
+import com.teslagov.joan.core.TokenManager;
 import org.apache.http.client.HttpClient;
+
+import java.time.ZoneOffset;
 
 /**
  * @author Kevin Chen
  */
-public class ArcPortalLogApi
+public class ArcPortalLogApi extends AbstractArcRestApi
 {
-	private final HttpClient httpClient;
-
-	private final ArcConfiguration arcConfiguration;
-
-	public ArcPortalLogApi( HttpClient httpClient, ArcConfiguration arcConfiguration )
+	public ArcPortalLogApi(
+		HttpClient httpClient,
+		ArcConfiguration arcConfiguration,
+		ZoneOffset zoneOffset,
+		TokenManager tokenManager,
+		String apiName
+	)
 	{
-		this.httpClient = httpClient;
-		this.arcConfiguration = arcConfiguration;
+		super( httpClient, arcConfiguration, zoneOffset, tokenManager, apiName );
 	}
 
 	/**
@@ -23,6 +28,30 @@ public class ArcPortalLogApi
 	 * recovered after executing this operation.
 	 */
 	public void cleanLogs()
+	{
+
+	}
+
+	/**
+	 * Returns the current log settings for the portal.
+	 */
+	public void getLogSettings()
+	{
+
+	}
+
+	/**
+	 * Updates the log settings for the portal.
+	 */
+	public void editLogSettings()
+	{
+
+	}
+
+	/**
+	 * The query operation allows you to aggregate, filter, and page through logs written by the portal.
+	 */
+	public void queryLogs()
 	{
 
 	}
